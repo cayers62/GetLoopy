8e91da3cbd896c0c5900855c6d6fc7c9cc187269
cjayers <ayerscj@mail.uc.edu>
2024-02-25 22:31:42 -0500

    Initial commit

---
+++ src/GetLoopy.java
@@ -0,0 +1,85 @@
+public class GetLoopy
+{
+    public static void main(String[] args) {
+
+
+        //Declare variable
+
+        int t = 0;
+        final String s1 = "";
+
+        //Counting to 30
+
+        for (t = 0; t <= 30; t++) {
+
+            System.out.printf("%3d  ", + t);
+        }
+
+        System.out.println("\n");
+
+        //counting down from 30
+
+        for (t = 30; t >= 0; t--){
+
+            System.out.printf("%3d  ", + t);
+        }
+            System.out.println("\n");
+
+        for ( t = 0; t <= 18; t += 3){
+
+            System.out.printf("%3d  ", + t);
+        }
+             System.out.println("\n");
+
+
+        for (t = 10; t >= 0; t -= 2){
+
+            System.out.printf("%3d  ", + t);
+
+        }
+            System.out.println("\n");
+
+
+               // declare a variable
+
+
+
+                for(int row1 = 1; row1 <= 5; row1++){
+
+                    for(int col1 = 1; col1 <= 5; col1++){
+
+                        System.out.println("*");
+                    }
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
