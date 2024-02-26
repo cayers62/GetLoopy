d5f82d32f36844901f0482dae435cc48149b337a
cjayers <ayerscj@mail.uc.edu>
2024-02-25 22:31:42 -0500

    redo to resubmit2

---
+++ src/GetLoopy.java
@@ -0,0 +1,55 @@
+
+
+public class GetLoopy
+{
+    public static void main(String[] args) {
+
+
+
+
+
+        for(int row1 = 5; row1 >= 0; row1--){
+
+            System.out.print("*");
+            System.out.println();
+
+            for(int col1 = 5;  col1 >= row1; col1--){
+
+                System.out.print("*");
+
+
+
+
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
